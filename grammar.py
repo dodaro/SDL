191c716e524d16c0a1662bd1428bd34892988b76
Ilaria <ila1402@icloud.com>
2024-02-12 17:36:00 +0100

    examples

--- grammar.py
+++ grammar.py
@@ -1,7 +1,7 @@
 from lark import Lark, Transformer, exceptions, Tree
 
 entities = {} #dizionario con chiave nome dell'entità e valore la lista dei parametri e rispettivi tipi
-guess=[] #lista degli alias ed entità definite nel from e nel with del costrutto guess
+guess={} #dizionario degli alias ed entità definite nel from e nel with del costrutto guess
 
 grammar = """
 	start:  _statement+
@@ -69,6 +69,9 @@ grammar = """
 """
 
 class DeclarationTransformer(Transformer):
+	def __init__(self):
+		self.count_guess=0
+		guess[0]=[]
 	def entity_declaration(self, args):
 		entity_name= args[0]
 		declarations=args[2].children
@@ -87,20 +90,22 @@ class DeclarationTransformer(Transformer):
 			attr[0].type=str(attr_type) #salvo il tipo dell'attributo nel token
 			entities[entity_name].append(attr[0]) #aggiungo nella lista degli attributi
 		return args
+	def guess(self, args):
+		self.count_guess+=1
+		guess[self.count_guess]=[]
 	def guess_as(self, args):
-		if(args[0] in guess):
+		if(args[0] in guess[self.count_guess]):
 			raise ValueError(f"Alias already defined: {args[0]}")
-		guess.append(args[0])
-		return args
+		guess[self.count_guess].append(args[0])
 	def guess_entity(self, args):
 		if(len(args)>1):
-			if(args[1] in guess):
+			if(args[1] in guess[self.count_guess]):
 				raise ValueError(f"Alias already defined: {args[1]}")
-			guess.append(args[1])
+			guess[self.count_guess].append(args[1])
 		else:
-			if(args[0] in guess):
+			if(args[0] in guess[self.count_guess]):
 				raise ValueError(f"Entity already defined: {args[0]}")
-			guess.append(args[0])
+			guess[self.count_guess].append(args[0])
 
 class CheckTransformer(Transformer):
 	def __init__(self):
@@ -110,6 +115,7 @@ class CheckTransformer(Transformer):
 		self.defined_node="" #entità definita nella define
 		self.redefined_node="" #alias dell'entità definita nella define
 		self.with_attributes=[] #lista contenente gli attributi delle entità definite nel with
+		self.count_guess=0
 	def custom_type(self, args):
 		if(not args[0] in entities.keys()):
 			raise ValueError(f"Unrecognized custom type: {args[0]}")
@@ -133,6 +139,11 @@ class CheckTransformer(Transformer):
 		self.defined_entities=set()
 		self.attributes={}
 		self.defined_node=args[0]
+		attr=entities[args[0]]
+		all_attr=[]
+		for i in range(len(attr)):
+			all_attr.append(attr[i]) 
+		self.attributes[self.defined_node]=all_attr
 		if(len(args)>1):
 			return f"define {args[0]} {args[1]} \n"
 		return f"define {args[0]} \n"
@@ -169,10 +180,14 @@ class CheckTransformer(Transformer):
 		return args
 	def args_type(self, args):	
 		return args[0]
-	def where_statement(self, args):	
+	def where_statement(self, args):
+		cond=args[0]==self.redefined_node or args[0]==self.defined_node	
 		if(len(args)>3):
-			if(not args[0] in self.declared_alias.keys() and not args[0] in self.defined_entities):
-				raise ValueError(f"Undefined entity or alias: {args[0]}")
+			if(cond):
+				args[0]=self.defined_node
+			else:
+				if(not args[0] in self.declared_alias.keys() and not args[0] in self.defined_entities):
+					raise ValueError(f"Undefined entity or alias: {args[0]}")
 			if(not args[2].value in self.attributes[args[0]]):
 				raise ValueError(f"Attribute {args[2]} of entity {args[0]} is not defined")
 			args_4=self.args_type(args[4])
@@ -193,19 +208,21 @@ class CheckTransformer(Transformer):
 						if(find):
 							raise ValueError(f"The reference to the attribute {args[0]} is ambiguous")
 						find=True
-				if(not find):
-					raise ValueError(f"Attribute {args[0]} is not defined")
+			if(not find and not (args[0] in self.declared_alias.keys() or args[0] in self.defined_entities or cond)):
+				if(not any(args[0]==attr.value for attr in self.attributes[self.defined_node])):
+						raise ValueError(f"Attribute {args[0]} is not defined")
 			args_2=self.args_type(args[2])
 			if(len(args[2])>1):
 				for i in range(1, len(args[2])):
 					args_2+=args[2][i]
 			return f"{args[0]} {args[1]} {args_2}"
 	def guess(self, args):
+		self.count_guess+=1
 		return f"{args[0]} {args[1]}\n{args[2]}\n  {args[3]}{args[4]}\n"
 	def guess_statement(self, args):
 		return f"guess "+ self.print_stat(args)
 	def guess_definition(self, args):
-		if not any(args[0]==t.value for t in guess):
+		if not any(args[0]==t.value for t in guess[self.count_guess]):
 			raise ValueError(f"{args[0]} is not defined")
 		return self.print_stat(args)
 	def from_guess(self, args):
@@ -232,7 +249,7 @@ class CheckTransformer(Transformer):
 		else:
 			if(args[0].type!="NAME"):
 				name=False
-		if(name and not args[0] in guess):
+		if(name and not args[0] in guess[self.count_guess]):
 			raise ValueError(f"{args[0]} is not defined")
 		return stat
 	def condition(self, args):
@@ -273,7 +290,7 @@ class CheckTransformer(Transformer):
 				open_bracket=True
 				token=args[i+3]
 				if(token[0].type=="NAME"):
-					if(not (token[0].value in guess)):
+					if(not (token[0].value in guess[self.count_guess])):
 						raise ValueError(f"{token[0].value} is not defined")
 				temp=args[i+3]
 				args[i+3]=""
@@ -286,7 +303,7 @@ class CheckTransformer(Transformer):
 		for i in range(len(args)):
 			if(args[i]!=","):
 				statements+=args[i]
-				if(not args[i] in guess):
+				if(not args[i] in guess[self.count_guess]):
 					raise ValueError(f"{args[i]} is not defined")
 			else:
 				statements+=", "
@@ -304,26 +321,9 @@ class CheckTransformer(Transformer):
 
 
 if __name__ == '__main__':
-	code="""
-@entity color: value: any;
-@entity node: value: int;
-@entity edge: node1: node, node2: node;
-@entity assign: node: node, color: color;
-
-define node as n
-    from edge, edge as e2
-	where node1=n and e2.node1=n;
-
-define color as c
-    from assign, node as n
-	where color=c and value=5;
-
-
-guess exactly 1 a: (c) with assign(node=n, color=c) as a 
-	from node as n, color as c
-		where n
-
-"""
+	code=""
+	with open('examples/example1.txt', 'r') as file:
+	    code = file.read()
 	try:
 		parser_entities = Lark(grammar, parser='lalr', transformer=DeclarationTransformer())
 		parser_entities.parse(code)
