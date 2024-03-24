053e4969ce931ef9351e3b9df017e8907f5cda6f
Ilaria <ila1402@icloud.com>
2024-03-24 19:32:28 +0100

    added tests

--- tests/oodl/test_grammar.py
+++ tests/oodl/test_grammar.py
@@ -7,13 +7,13 @@ import pytest
 
 from grammar import build_tree
 
-
 def assert_in_transformation(code: str, content: str):
     def normalize(string):
         res = string.strip().replace("\t", "    ")
         res = re.sub(r"problem(\d+)\+=", "problem+=", res)
         return res
     tree = normalize(str(build_tree(code)))
+    print(tree,"\n", normalize(content))
     assert normalize(content) in tree
 
 
@@ -35,6 +35,18 @@ define Node as n
 	where Edge.node1=n and e2.node1=n;""", 
 """with Node() as n_0, Edge(node1 = n_0) as e_1, Edge(node1 = n_0) as e2_2:
 	problem+=When(e2_2, e_1).define(n_0)""")
+
+def test_build_tree_define_2():
+	assert_in_transformation("""
+@entity Person: name: str, age: int;
+@entity Car: brand: str, model: str;
+@entity Ownership: owner: Person, car: Car;
+
+define Person as pc
+    from Car, Ownership
+    where Ownership.owner = pc and Car.brand="Nissan";""", 
+"""with Person() as pc_0, Car() as c_1, Ownership(owner = pc_0) as o_2:
+	problem+=When(o_2, c_1, c_1.brand=="Nissan").define(pc_0)""")
 
 
 def test_build_tree_aggregate():
@@ -84,7 +96,7 @@ guess from User as user, Bit_rate as b, Video as v
 			from F as f
 				where f.bit_rate.value<=user.max_bit.value and f.video=user.video and f.resolution=user.resolution and a.user=user and a.bit_rate=f.bit_rate and a.sat_value=f.sat_value;""", 
 """with Bit_rate() as b_2, Video() as v_3, User(max_bit = b_2) as user_1, F(video = user_1.video, resolution = user_1.resolution) as f_4, Assign_(user = user_1, bit_rate = f_4.bit_rate, sat_value = f_4.sat_value) as a_0:
-	problem+=When(user_1, b_2, v_3, user_1.max_bit.value==2).guess({a_0:(f_4, f_4.bit_rate . value <= user_1.max_bit.value)})""")
+	problem+=When(user_1, b_2, v_3, user_1.max_bit.value==2).guess({a_0:(f_4, f_4.bit_rate.value<=user_1.max_bit.value)})""")
 	assert_in_transformation("""
 guess from F, Resolution as r
 	where F.resolution.value=1 and F.resolution=r and r.value<=4
@@ -95,11 +107,10 @@ guess from F, Resolution as r
 				from Assign_ as a3
 					where a3.sat_value=Sat and a3.sat_value.value=2;""",
 """with Resolution() as r_3, Bit_rate() as b2_0, Sat() as s_1, Assign_(sat_value = s_1) as a3_4, F(resolution = r_3) as f_2:
-	problem+=When(f_2, r_3, f_2.resolution.value==1, r_3.value<=4).guess({b2_0:(b2_0.value == f_2.resolution.value), s_1:(a3_4, a3_4.sat_value . value == 2)}, at_most=1, at_least=f_2.resolution.value)""")
+	problem+=When(f_2, r_3, f_2.resolution.value==1, r_3.value<=4).guess({b2_0:(b2_0.value==f_2.resolution.value), s_1:(a3_4, a3_4.sat_value.value==2)}, at_most=1, at_least=f_2.resolution.value)""")
 
-def test_build_tree_deny_unless_otherwise():
+def test_build_tree_assert_otherwise():
 	assert_in_transformation("""
-
 @entity Book: title: str, author: str, genre: str, publication_year: int;
 @entity Reader: name: str, age: int, favorite_genre: str;
 @entity Review: book: Book, reader: Reader, rating: int, comment: str;
@@ -109,6 +120,17 @@ deny unless Book as b or Reader as r
 			or pay review.rating at 5;""",
 """with Book() as b_0, Reader() as r_1, Review() as review_2:
 	problem+=Assert(b_0, r_1).when(review_2, b_0.genre=="Horror", r_1.age==19).otherwise(review_2.rating,5,0)""")
+
+def test_build_tree_assert_otherwise_aggregate():
+	assert_in_transformation("""
+@entity Book: title: str, author: str, genre: str, publication_year: int;
+@entity Reader: name: str, age: int, favorite_genre: str;
+@entity Review: book: Book, reader: Reader, rating: int, comment: str;
+deny unless Book as b or Reader as r
+	sum of {review from Review as review} == 3
+		or pay r.age at 5;""",
+"""with Book() as b_0, Reader() as r_1, Review() as review_2:
+	problem+=Assert(b_0, r_1).when(Sum({(review_2): (review_2)})==3).otherwise(r_1.age,5,0)""")
 
 def test_build_tree_deny():
 	assert_in_transformation("""
@@ -121,7 +143,18 @@ deny from Student, Course as c, Enrollment as e
 """with Student() as s_0, Course() as c_1, Enrollment(student = s_0, course = c_1) as e_2:
 	problem+=Assert(False).when(s_0, c_1, e_2, c_1.credits==2)""")
 
-def test_build_tree_deny_unless():
+def test_build_tree_deny_aggregate():
+	assert_in_transformation("""
+@entity Student: name: str, age: int, grade: str;
+@entity Course: name: str, credits: int;
+@entity Enrollment: student: Student, course: Course;
+
+deny from Student, Course as c
+	count of {e.course from Enrollment as e where e.student=Student} > 2;""",
+"""with Student() as s_0, Course() as c_1, Enrollment(student = s_0) as e_2:
+	problem+=Assert(False).when(s_0, c_1, Count({(e_2.course): (e_2)})>2)""")
+
+def test_build_tree_assert():
 	assert_in_transformation("""
 @entity Customer: name: str, email: str, age: int;
 @entity Product: name: str, price: int, category: str;
@@ -132,6 +165,19 @@ deny unless Order as o
 		where o.customer=c and o.product=p and o.quantity=200 and p.price=20;""",
 """with Product() as p_1, Customer() as c_2, Order(customer = c_2, product = p_1) as o_0:
 	problem+=Assert(o_0).when(p_1, c_2, o_0.quantity==200, p_1.price==20)""")
+	
+def test_build_tree_assert_aggregate():
+	assert_in_transformation("""
+@entity Customer: name: str, email: str, age: int;
+@entity Product: name: str, price: int, category: str;
+@entity Order: customer: Customer, product: Product, quantity: int, date: str;
+
+deny unless Customer as c
+	from Product as p
+		count of {o.customer, o.product from Order as o where o.product=p} == 2
+			where p.price=20;""",
+"""with Customer() as c_0, Product() as p_1, Order(product = p_1) as o_2:
+	problem+=Assert(c_0).when(p_1, p_1.price==20, Count({(o_2.customer, o_2.product): (o_2)})==2)""")
 
 def test_build_tree_deny_otherwise():
 	assert_in_transformation("""
@@ -143,3 +189,14 @@ deny from Task as t, Project as p, Employee as e
 		or pay e.salary at 3;""",
 """with Project() as p_1, Employee() as e_2, Task(project = p_1, assigned_to = e_2) as t_0:
 	problem+=Assert(False).when(t_0, p_1, e_2, e_2.salary<1000).otherwise(e_2.salary,3,0)""")
+
+def test_build_tree_deny_otherwise_aggregate():
+	assert_in_transformation("""
+@entity Employee: name: str, department: str, position: str, salary: int;
+@entity Project: name: str, department: str, deadline: str;
+@entity Task: project: Project, assigned_to: Employee, description: str, status: str;
+deny from Task as t, Project as p
+	count of {t2.project from Task as t2 where t2.project.department=t.project.department} > 1
+		or pay t.assigned_to.salary at 3;""",
+"""with Task() as t_0, Project() as p_1, Task() as t2_2:
+	problem+=Assert(False).when(t_0, p_1, Count({(t2_2.project): (t2_2, t2_2.project.department==t_0.project.department)})>1).otherwise(t_0.assigned_to.salary,3,0)""")
